f20e1341f37afef73f044f8767d4e8c5bb43efe5
nlopez <nelly.lopez@utexas.edu>
2021-04-02 16:53:39 -0500

    updated app.js in client

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Rooms from './pages/Rooms';
+import Error from './pages/Error';
+import { Route, Switch, Link, BrowserRouter as Router } from "react-router-dom";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <>
+    <Router>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/rooms">Rooms</Link>
+          </li>
+          <li>
+            <Link to="/error">Error</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/rooms">
+          <Rooms />
+        </Route>
+        <Route path="/error">
+          <Error />
+        </Route>
+      </Switch>
+      </div>
+       </Router>
+    </>
+  );
+}
+
+export default App;
+
+
+<header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,8 +66,3 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
-  );
-}
-
-export default App;
