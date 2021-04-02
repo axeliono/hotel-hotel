2d920884b863af244e52157117f284af3629a973
axeliono <69375446+axeliono@users.noreply.github.com>
2021-04-01 20:15:07 -0500

    created models, moving on to testing typeDefs/resolvers in graphql

---
+++ server/models/User.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+const extendSchema = require("mongoose-extend-schema");
+
+const { Schema } = mongoose;
+const bcrypt = require("bcrypt");
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
+  roomNumber: {
+    type: Schema.Types.ObjectId,
+    ref: "Room",
+  },
+});
+
+const AdminUserSchema = extendSchema(userSchema, {
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+});
+
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+userSchema.methods.isCorrectPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
+const Admin = mongoose.model("Admin", AdminUserSchema);
+
+module.exports = { User, Admin };
