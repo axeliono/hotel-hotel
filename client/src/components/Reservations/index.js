42f38d50e0e9201170cc1ab2a7f5181ba63a5f5a
Obinna Ezekwe <ozekwe@gmail.com>
2021-04-07 02:03:35 -0500

    Draft of reservation page

--- client/src/components/Reservations/index.js
+++ client/src/components/Reservations/index.js
@@ -1,11 +1,35 @@
 import React from 'react';
-import styled from 'styled-components';
+import './style.css';
 
 
 
 export default function Reservations() {
     return (
-        <>
-        </>
+        <section>
+          <form id="reservation-form">
+            <div>
+                <label htmlFor="firstName">First Name:</label>
+                <input type="text" name="firstName" />
+            </div>
+            <div>
+                <label htmlFor="lastName">Last Name:</label>
+                <input type="text" name="lastName" />
+            </div>
+            <div>
+                <label htmlFor="email">Email address:</label>
+                <input type="email" name="email" />
+            </div>
+            <div>
+                <label htmlFor="checkInDate">Check-In Date:</label>
+                <input type="date" name="checkInDate" />
+            </div>
+            <div>
+                <label htmlFor="checkOutDate">Check-Out Date:</label>
+                <input type="date" name="checkOutDate" />
+            </div>
+
+            <button data-testid="button" type="submit">Submit</button>
+          </form>
+        </section>
     )
 };
