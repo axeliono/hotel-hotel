570c5eb3c88c266103b9f265778c736d03440520
RickySulli <71475646+RickySulli@users.noreply.github.com>
2021-04-11 21:14:19 -0500

    redo from this afternoon

--- client/src/components/Banner/index.js
+++ client/src/components/Banner/index.js
@@ -7,7 +7,7 @@ justify-content: center;
 align-items: center;
 height: 100%;
 width: 100%;
-border-image: (https://i.imgur.com/dREL3hO.png);
+
 `;
 
 export default function Banner({title, subtitle}) {
